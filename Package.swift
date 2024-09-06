ad58229a99557714603cf9e47b354eb27206e982
Luiz Sena <senaluiz1307@gmail.com>
2024-09-05 23:28:35 -0300

    upload project

--- Package.swift
+++ Package.swift
@@ -4,18 +4,20 @@ import PackageDescription
 let package = Package(
     name: "wpp-server",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/funcmike/rabbitmq-nio.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
+                .product(name: "AMQPClient", package: "rabbitmq-nio"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//
+//
+//  Created by Luiz Sena on 02/09/24.
+//
+
+import Vapor
+import Foundation
+
+struct User {
+    let id: String
+    let ws: WebSocket
+}
